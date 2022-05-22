15840a651b612c159c8f4ff7e7cf4abcf240505f
cryptomehedi <classicalmehedi@gmail.com>
2022-05-22 17:29:21 +0600

    project start

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      'Poppins': ['Poppins', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
